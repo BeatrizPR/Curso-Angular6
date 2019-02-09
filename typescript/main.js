86e2963f62305854366e7f4c42aa3dec82a32842
Beatriz Parejo Ramos <32536069+BeatrizPR@users.noreply.github.com>
2019-02-09 17:01:35 +0100

    typescript

---
+++ typescript/main.js
@@ -0,0 +1,36 @@
+"use strict";
+maxValue(3, 9);
+function maxValue(value1, value2) {
+    return Math.max(value1, value2);
+}
+// definición de tipos
+// boolean
+var isBoolean = false;
+//Number
+var numerillo = 43.320;
+var age = 25;
+// string
+var myName = 'Beatriz';
+// array
+var listAges = [20, 19, 35, 43];
+var listNames = ['Juan', 'Ana', 'Pedro', 'Paloma'];
+// tupla - array de clave-valor
+var personTuple;
+personTuple = ['Samuel', 30];
+// enums
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+    Color[Color["Pink"] = 3] = "Pink";
+    Color[Color["Yellow"] = 4] = "Yellow";
+    Color[Color["Purple"] = 5] = "Purple";
+    Color[Color["Brown"] = 6] = "Brown";
+})(Color || (Color = {}));
+Color.Blue;
+// no se podría poner un color que no tengamos en el enum
+// Any
+var notSure = '298';
+notSure = 80;
+notSure = true;
---
+++ typescript/main.ts
@@ -0,0 +1,52 @@
+maxValue(3, 9);
+
+function maxValue(value1: number, value2: number){
+    return Math.max(value1, value2);
+}
+
+// definición de tipos
+
+// boolean
+
+let isBoolean: Boolean = false;
+
+//Number
+
+let numerillo: number = 43.320;
+
+let age: number = 25;
+
+// string
+let myName: string = 'Beatriz';
+
+// array
+
+let listAges:number[] = [20, 19, 35, 43];
+let listNames: Array<string> =['Juan', 'Ana', 'Pedro', 'Paloma'];
+
+// tupla - array de clave-valor
+
+let personTuple:[string, number];
+personTuple = ['Samuel', 30];
+
+// enums
+
+enum Color{
+    Red, 
+    Green, 
+    Blue,
+    Pink,
+    Yellow, 
+    Purple,
+    Brown
+}
+
+Color.Blue;
+// no se podría poner un color que no tengamos en el enum
+
+// Any
+
+let notSure: any = '298';
+notSure = 80;
+notSure = true;
+
