3e34b8f549f34a0f09e4edd5afecd21841974d83
Beatriz Parejo Ramos <32536069+BeatrizPR@users.noreply.github.com>
2019-02-10 01:53:26 +0100

    1 application

---
+++ application-openwebinars/src/app/application-info/application-info.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-application-info',
+  // template: `
+  // <p>
+  // Esto es una prueba.
+  // </p>
+  // `,
+  // styles:[
+  //   `
+  //   p{
+  //     color: blue;
+  //   }
+  //   `
+  // ]
+  templateUrl: './application-info.component.html',
+  styleUrls: ['./application-info.component.css']
+})
+export class ApplicationInfoComponent implements OnInit {
+
+  constructor() {
+   }
+
+  ngOnInit() {
+  }
+
+}
