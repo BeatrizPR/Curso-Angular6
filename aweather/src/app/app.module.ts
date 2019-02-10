5c9d0ab8ac317e25f616eb2a675099a6fd0f05fa
Beatriz Parejo Ramos <32536069+BeatrizPR@users.noreply.github.com>
2019-02-10 18:24:46 +0100

    weather

--- aweather/src/app/app.module.ts
+++ aweather/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherCardComponent } from './weather-card/weather-card.component';
+import { MatCardModule, MatButtonModule, MatDividerModule, MatGridListModule, MatProgressSpinnerModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatGridListModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
